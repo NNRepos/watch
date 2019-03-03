7cc1c4fa6947caa9b70dcea0c9cd8a8717b5c2ee
NNrepos <nivimano@gmail.com>
2019-03-04 00:11:57 +0200

    commiting after deleting requirements

--- install.cmd
+++ install.cmd
@@ -1,19 +1,13 @@
 @echo off
 python -V
 if errorlevel 1 goto noPython
-pip -V
-if errorlevel 1 goto noPip
 goto success
 :noPython
 echo Error: No python 2 installed.
 goto end
-:noPip
-echo Error: No pip installed.
-goto end
 :success
-pip install -r "%~dp0files/requirements.txt"
 python "%~dp0files/moveAll.py"
 python "%~dp0watch.py" -s
 goto eof
 :end
-set /p a=
+set /p a= ::this will be removed
