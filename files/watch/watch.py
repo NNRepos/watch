48902df66626034581b3d0345477f25b30fe6774
NNrepos <nivimano@gmail.com>
2019-03-05 20:12:48 +0200

    finished play, get_next_episode, etc.. I've done some basic testing but I need to create tests, a branch, and an installation for linux(to run on circleci).

--- files/watch/watch.py
+++ files/watch/watch.py
@@ -5,21 +5,23 @@ import csv
 import json
 import sys
 import re
-from os import path
+from os import path, walk
 from datetime import datetime
+from subprocess import Popen
 
 #parameters
 DEFAULT_VLC_PATH = r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
 USER_HOME = path.expanduser('~')
+VIDEO_FILE_EXTENSIONS = r"(webm|mkv|flv|vob|ogv|ogg|drc|gifv|mng|avi|mts|m2ts|mov|qt|wmv|yuv|rm|rmvb|asf|amv|mp4|m4p|m4v|mpg|mp2|mpeg|mpe|mpv|m2v|svi|3gp|3g2|mxf|roq|nsv|f4v|f4p|f4a|f4b)" #no gifs
 
 def watch():
     try:
+        cwd = path.dirname(path.realpath(__file__)) #watch.py's directory
+        settings_path = path.join(cwd, "settings")
+        db, cursor = get_db(cwd) #create db in watch.py's directory
         assert sys.platform.startswith('win'), "watch.py is for windows only."
         args = parse() #-h stops here
-        #get settings and db
-        cwd = path.dirname(path.realpath(__file__))
-        settings_dict = get_settings(args.settings_flag, cwd)
-        db, cursor = get_db(cwd)
+        settings_dict = get_settings(args.settings_flag, settings_path)
         #get whole db
         if args.view_flag or args.export_flag: #we need whole DB for both
             cursor.execute("SELECT * FROM series")
@@ -29,7 +31,7 @@ def watch():
             view(db_items)
         #settings
         if args.settings_flag:
-            settings_dict = settings(settings_dict, args.cli_flag, cwd)
+            settings_dict = settings(settings_dict, args.cli_flag, settings_path)
         #add
         if args.add_flag:
             add(db, cursor, args.cli_flag)
@@ -41,26 +43,56 @@ def watch():
             remove(db, cursor, args.cli_flag)
         #export
         if args.export_flag:
-            if 'export id' not in settings_dict:
-                settings_dict['export id'] = 0
-            settings_dict['export id'] += 1
-            export_file_name = "series" + "{:>03}".format(settings_dict['export id']) + ".csv"
-            export_file_path = path.join(settings_dict['export path'], export_file_name)
-            export(export_file_path, db_items)
-        #watch series
-        if args.series_name: #TODO
+            export(settings_dict, settings_path, db_items)
+        #get series data
+        prepared = False
+        
+        #if specified in args
+        if args.series_name: 
+            series_dict = get_series_from_db(cursor, args.series_name)
             if args.season:
                 if args.episode: #watch <series_name> S<season>E<episode>
-                    pass
+                    try:
+                        season = int(args.season)
+                        episode = int(args.episode)
+                    except:
+                        raise Exception('season and episode must be integers')
+                    total = series_dict['total_episodes_watched']
+                    prepared = True
                 else: #season but no episode
                     raise Exception("A season must be followed by an episode.")
-            else: #get last episode from sql etc.
-                pass 
-        #finish
-        db.close()
+            else: 
+                episode, season, _, total = get_next_episode(series_dict)
+                prepared = True
+        
+        #if no other args specified
+        elif not (args.add_flag or args.edit_flag or 
+            args.remove_flag or args.settings_flag or 
+            args.export_flag or args.view_flag):
+            if 'last series watched' in settings_dict:
+                last_series = settings_dict['last series watched']
+                series_dict = get_series_from_db(cursor, last_series)
+                episode, season, _, total = get_next_episode(series_dict)
+                prepared = True
+            else:
+                raise Exception("To watch a series for the first time, use watch <name> or watch <name> <season> <episode>")
+            
+        #actually watch
+        if prepared:
+            settings_dict['last series watched'] = series_dict['name']
+            series_dict['last_episode_watched'] = episode
+            series_dict['last_season_watched'] = season
+            series_dict['total_episodes_watched'] = total + 1
+            if season<1 or episode<1:
+                raise Exception('season and episode must be positive')
+            edit(db, cursor, prompt=False, series_dict=series_dict)
+            write_settings(settings_path, settings_dict)
+            play(settings_dict, series_dict)
     except Exception as e:
-        print "Error:", e
+        print "Error: ", e #, "at", sys.exc_info()[2].tb_lineno
         sys.exit(1)
+    finally:
+        db.close()
 
 
 def parse():
@@ -71,11 +103,11 @@ def parse():
         "watch -a, and watch it with watch <series_name>. Some examples:\n" +
         "1. watch GoT 1 4 : Watch first season, fourth episode of the series you added as 'got'.\n"
         "2. watch Vikings : Watch the next episode of the series you added as 'vikings'.\n" +
-        "3. watch -ve : Print and export your watch stats.",
+        "3. watch -vx : Print and export your watch stats.",
         epilog='it is recommended to set up the settings, add a series, and then use watch \r\nwithout parameters until the last episode of the series',
-        usage='watch [series_name [season episode]] [-h] [-a | -e | -r | -s | -x] [-v] [-c] [-d]',
+        usage='watch [series_name [season episode]] [-h] [-a | -e | -r | -s | -x] [-v] [-c]',
         formatter_class=argparse.RawDescriptionHelpFormatter)
-
+    
     group1 = parser.add_mutually_exclusive_group()
     group1.add_argument('-a','--add', help="add a new series to watch", action='store_true', dest='add_flag')
     group1.add_argument('-e','--edit', help="edit a series you've already added, keeping the last and total episodes watched", action='store_true', dest='edit_flag')
@@ -88,7 +120,7 @@ def parse():
     parser.add_argument('season', help="the season to be watched", nargs='?', type=int)
     parser.add_argument('episode', help="the episode to be watched", nargs='?', type=int)
     parser.add_argument('-c','--cli', help="use with add, settings, or export, to open them in a command line interface", action='store_true', dest='cli_flag')
-    parser.add_argument('-d','--debug', help="print file name instead of watching it, without saving to database", action='store_true', dest='debug_flag')
+    
     return parser.parse_args()
 
 
@@ -103,10 +135,12 @@ def add(db, cursor, cli):
         for k,v in new_series.iteritems():
             prompt_text = unslugify(str(k)) + ':'
             new_series[k] = raw_input(prompt_text)
+            if k=='path':
+                new_series[k] = new_series[k].strip(''''"''')
     else: #GUI
         pass
     new_series['last_season_watched'] = 1
-    new_series['last_episode_watched'] = 1
+    new_series['last_episode_watched'] = 0
     new_series['total_episodes_watched'] = 0
     new_series['date_added'] = datetime.now().strftime("%B %m, %Y, %H:%M:%S")
     cursor.execute('''INSERT INTO series(name, "full name", path, "last season watched", "last episode watched", "total episodes watched", "date added")
@@ -115,57 +149,55 @@ def add(db, cursor, cli):
     db.commit()
 
 
-def edit(db, cursor, cli):
-    if cli: #TODO: use if cli else in between common lines
-        series_name = raw_input("Enter the name(short) of the series you want to edit(use watch -v to view your series):")
-        cursor.execute('''SELECT * FROM series WHERE name=?''', (series_name,))
-        result = cursor.fetchone()
-        if not result:
-            raise Exception("no series with such name.")
-        dict_keys = map(slugify, result.keys())
-        series = dict(zip(dict_keys, result))
-        print "Type in each value to edit the field, leave blank for no changes (old value in parentheses)"
-        for k,v in series.iteritems():
-            if k=='id':
-                continue
-            prompt_text = unslugify(str(k)) + '(' + str(v) + '):'
-            new_value = raw_input(prompt_text)
-            if k=='last_episode_watched' or k=='last_season_watched' or k=='total_episodes_watched':
-                try:
-                    int(new_value)
-                except Exception:
-                    raise Exception(str(k) + " has to be a number.")
-            series[k] = new_value or v
-    else: #GUI
-        pass
+def edit(db, cursor, cli=False, prompt=True, series_dict=None):
+    """Edits a DB row with user interference(prompt=True),
+    or without it (prompt=False, series_dict!=None).
+    series_dict must be slugified.
+    """
+    if prompt:
+        if cli: #TODO: use if cli else in between common lines
+            series_name = raw_input("Enter the name(short) of the series you want to edit(use watch -v to view your series):")
+            series_dict = get_series_from_db(cursor, series_name)
+            print "Type in each value to edit the field, leave blank for no changes (old value in parentheses)"
+            for k,v in series_dict.iteritems():
+                if k=='id' or k=='total_episodes_watched':
+                    continue
+                prompt_text = unslugify(str(k)) + '(' + str(v) + '):'
+                new_value = raw_input(prompt_text)
+                if k=='path':
+                    series_dict[k] = series_dict[k].strip(''''"''')
+                if new_value and (k=='last_episode_watched' or k=='last_season_watched' or k=='total_episodes_watched'):
+                    try:
+                        int(new_value)
+                    except Exception:
+                        raise Exception(str(k) + " has to be a number.")
+                series_dict[k] = new_value or v
+        else: #GUI
+            pass
     #store in db
-    cursor.execute('''DELETE FROM series WHERE name=?''', (series_name,))
+    cursor.execute('''DELETE FROM series WHERE name=?''', (series_dict['name'],))
     cursor.execute('''INSERT INTO series(name, "full name", path, "last season watched", "last episode watched", "total episodes watched", "date added")
         VALUES(:name, :full_name, :path, :last_season_watched, :last_episode_watched, :total_episodes_watched, :date_added)
-    ''', series)
+    ''', series_dict)
     db.commit()
-    raw_input("edit successful.")
+    if prompt:
+        raw_input("edit successful.")
 
 
 def remove(db, cursor, cli):
     if cli: #TODO: use if cli else in between common lines
-        series_name = raw_input("Enter the name(short) of the series you want to edit(use watch -v to view your series):")
-        cursor.execute('''SELECT * FROM series WHERE name=?''', (series_name,))
-        result = cursor.fetchone()
-        if not result:
-            raise Exception("no series with such name.")
-        
+        series_name = raw_input("Enter the name(short) of the series you want to remove(use watch -v to view your series):")
     else: #GUI
         pass
+    series_dict = get_series_from_db(cursor, series_name)
     #remove from db
-    cursor.execute('''DELETE FROM series WHERE name=?''', (series_name,))
+    cursor.execute('''DELETE FROM series WHERE name=?''', (series_dict['name'],))
     db.commit()
     raw_input("removal successful.")
 
 
-def settings(settings_dict, cli, cwd):
+def settings(settings_dict, cli, settings_path):
     #TODO: GUI
-    settings_path = path.join(cwd, "settings")
     if settings_dict:
         old_settings_dict = settings_dict
         settings_dict = {}
@@ -187,16 +219,24 @@ def settings(settings_dict, cli, cwd):
     return settings_dict
 
 
-def export(export_file_path, db_items):
-    with open(export_file_path, "wb") as f:
-        if db_items:
-            writer = csv.writer(f)
-            writer.writerow(db_items[0].keys())
-            print len(db_items)
-            for row in db_items:
-                writer.writerow(row)
-        else:
-            f.write("The database is empty.")
+def export(settings_dict, settings_path, db_items):
+    if 'export id' not in settings_dict:
+        settings_dict['export id'] = 0
+    settings_dict['export id'] += 1
+    export_file_name = "series" + "{:>03}".format(settings_dict['export id']) + ".csv"
+    export_file_path = path.join(settings_dict['export path'], export_file_name)
+    try:
+        with open(export_file_path, "wb") as f:
+            if db_items:
+                writer = csv.writer(f)
+                writer.writerow(db_items[0].keys())
+                for row in db_items:
+                    writer.writerow(row)
+            else:
+                f.write("The database is empty.")
+    except:
+        raise Exception("The export path is incorrect")
+    write_settings(settings_path, settings_dict)
 
 
 def view(db_items):
@@ -208,7 +248,7 @@ def view(db_items):
                 print (str(k) + ": " + str(v)).center(80,' ')
             print ''.center(80,'_')
     else:
-        print "it seems like the database is empty."
+        print "looks like the database is empty."
     raw_input("press enter to proceed")
 
 
@@ -224,8 +264,7 @@ def write_settings(file, settings_dict):
     return
 
 
-def get_settings(flag, cwd):
-    settings_path = path.join(cwd, "settings")
+def get_settings(flag, settings_path): #TODO: make sure no1 using settings b4 they exist
     if path.isfile(settings_path):
         settings_dict = read_settings(settings_path)
     elif not flag:
@@ -263,5 +302,66 @@ def slugify(str):
 
 def unslugify(str):
     return re.sub('_', ' ', str)
+
+
+def get_series_from_db(cursor, series_name):
+    cursor.execute('''SELECT * FROM series WHERE name = ?''',
+        (series_name,))
+    result = cursor.fetchone()
+    if not result:
+        raise Exception("no series with such name: " + str(series_name))
+    dict_keys = map(slugify, result.keys())
+    series_dict = dict(zip(dict_keys, result))
+    return series_dict
+
+
+def find_file(series_path, season, episode):
+    target = r".*S0*" + str(season) + "E0*" + str(episode) + r"(\D.*|)\." + VIDEO_FILE_EXTENSIONS
+    found = False
+    for root, dirs, files in walk(series_path):
+        if found:
+            break;
+        for file in files:
+            if(re.match(target,file, flags=re.IGNORECASE)):
+                found=[episode, season, path.join(root,file)]
+    else: #nothing found
+        episode = 1
+        season += 1
+        target = r".*S0*" + str(season) + "E0*" + str(episode) + r"(\D.*|)\." + VIDEO_FILE_EXTENSIONS
+        for root, dirs, files in walk(series_path):
+            for file in files:
+                if(re.match(target,file, flags=re.IGNORECASE)):
+                    found=[episode, season, path.join(root,file)]
+    return found
+
+def get_next_episode(series_dict): #TODO
+    series_path = series_dict['path']
+    episode = series_dict['last_episode_watched'] + 1
+    season = series_dict['last_season_watched']
+    total = series_dict['total_episodes_watched']
+    found = find_file(series_path, season, episode)
+    if not found:
+        if total > 0: #was found before
+            raise Exception("CONGRATULATIONS! You have finished " + series_dict['full_name'] +
+            "!\r\nYou can add another series using watch -a, or look at your stats using watch -v")
+        else:
+            raise Exception("It seems like the path(" + series_path + ") is incorrect.\r\n" +
+                "Please change it using watch -e")
+    found.append(total)
+    return found
+
+def play(settings_dict, series_dict): #TODO
+    #get video
+    series_path = series_dict['path']
+    episode = series_dict['last_episode_watched']
+    season = series_dict['last_season_watched']
+    found = find_file(series_path, season, episode)
+    found_path = found[2]
+    #get player
+    player_path = settings_dict['player path']
+    #awaken
+    Popen([player_path,found_path],stdin=None, stdout=None, stderr=None)
+
+
 if __name__ == '__main__':
     watch()
